b501fa9b9ac15602e3bcd3a785c9c0a193c6b9e9
Grondhog <Sgronhovd@gmail.com>
2016-02-15 22:03:46 -0800

    First Commit
    Implemented 8-direction movement, built a couple of terrain prefabs.
    Next steps will be to add items, and using said items.

---
+++ Isometric Farming/Assets/Scripts/EightDirectionPlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EightDirectionPlayerController : MonoBehaviour {
+
+	[SerializeField]
+    private float speed = 5f;
+
+    private RotateGraphics rg;
+	
+	
+	void Start () {
+		rg = GetComponentInChildren<RotateGraphics>();
+		print(rg);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xMov = Input.GetAxisRaw("Horizontal");
+		float zMov = Input.GetAxisRaw("Vertical");
+		Vector3 direction = new Vector3(xMov, 0, zMov);
+		transform.Translate(direction * speed * Time.deltaTime);
+		rg.SetDirection(direction);
+		//transform.LookAt(direction);
+	}
+}
---
+++ Isometric Farming/Assets/Scripts/RotateGraphics.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateGraphics : MonoBehaviour {
+
+	private Vector3 direction = Vector3.zero;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!direction.Equals(Vector3.zero))
+		{
+			Quaternion rotation = Quaternion.LookRotation(direction);
+			transform.rotation = rotation;
+		}
+	}
+
+	public void SetDirection(Vector3 _direction)
+	{
+		direction = _direction;
+	}
+}
